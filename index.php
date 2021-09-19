f657a34aa7f59c75e1c4fe652aa289a657528d02
phuong32123 <www.phuong932001vn@gmail.com>
2021-09-19 13:58:17 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nhà Hàng</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+   <section class="navbar">
+       <div class="container">
+            <div class="logo"> 
+                <img src="images/logo.png" alt="Restaurant Logo" class="img-rsponsive">
+            </div>
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="a">Home</a>
+                    </li>
+                    <li>
+                        <a href="a">About</a>
+                    </li>
+                    <li>
+                        <a href="a">Foods</a>
+                    </li>
+                    <li>
+                        <a href="a">Contact</a>
+                    </li>
+                </ul>
+
+            </div>
+            <div class="clearfix"></div>
+       </div>
+   </section>
+
+   <section class="food-search text-center">
+   <div class="container">
+       <form action="">
+           <input lype="search" name="search" placholder="Search for food..">
+           <input type="submit" name="submit" vulue="search" class="btn btn-primary">
+</form>
+    </div>
+   </section> 
+
+   <section class="categories">
+   <div class="container">
+   <div class="container">
+        <h2 class="text-center">
+        Explore Foods
+        </h2>
+        <a href="">
+        <div class="box-3 float-container">
+             <img src="images/pizza.jpg" alt="Pizza" class="img-responsive img-curve">
+            <h3 class="float-text text-white">Pizza</h3>
+        </a>
+        </div>
+        <a href="">
+        <div class="box-3 float-container">
+        <img src="images/burger.jpg" alt="burger" class="img-responsive img-curve">
+            <h3 class="float-text text-white">burger</h3>
+
+        </div>        
+        </a>
+        <a href="">
+        <div class="box-3 float-container">
+        <img src="images/momo.jpg" alt="Momo" class="img-responsive img-curve">
+            <h3 class="float-text text-white">Momo</h3>
+        </div>
+        </a>
+        <div class="clearfix"></div>
+       </div>
+   </section> 
+
+   <section class="food-menu">
+   <div class="container">
+            <h2 class="text-center">Food Menu</h2>
+            
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>
+                        Food Title
+                    </h4>
+                    <p class="food-price">$2.3</p>
+                    <p class="food-detail">
+                        Made with Italian Sauce,Chicken,and organice vegetables.
+                    </p>
+                    <a href="a" class="btn btn-primary">Order Now</a>
+
+                </div>
+            </div>
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>
+                    Nice Burger                    
+                 </h4>
+                    <p class="food-price">$2</p>
+                    <p class="food-detail">
+                        Made with Italian Sauce,Chicken,and organice vegetables.
+                    </p>
+                    <a href="a" class="btn btn-primary">Order Now</a>
+
+                </div>
+            </div>
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-momo.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>
+                        Chicken Steam Mono
+                    </h4>
+                    <p class="food-price">$5</p>
+                    <p class="food-detail">
+                        Made with Italian Sauce,Chicken,and organice vegetables.
+                    </p>
+                    <a href="a" class="btn btn-primary">Order Now</a>
+
+                </div>
+            </div>
+        <div class="clearfix"></div>  
+       </div>
+   </section>
+
+   <section class="social">
+   <div class="container text-center">
+         <ul>
+             <li>
+                 <a hrf="a"><img src="https://img.icons8.com/color/48/000000/facebook.png"/></a>
+             </li>
+             <li>
+                 <a hrf="a"><img src="https://img.icons8.com/color/48/000000/instagram-new.png"/></a>
+             </li>
+             <li>
+                 <a hrf="a"><img src="https://img.icons8.com/color/48/000000/twitter--v1.png"/></a>
+             </li>
+         </ul>   
+   
+       </div>
+   </section>
+
+   <section class="footer">
+   <div class="container text-center">
+            <p>All rights resever.Designd By <a herf="a">Vijay thapa</p>
+       </div>
+   </section>  
+</body>
+</html>
